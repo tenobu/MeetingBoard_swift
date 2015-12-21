27f50d8ed15b97fd18807b40fdd7dd86e10f8885
寺内 信夫 <tenobu@gmail.com>
2015-12-22 05:29:28 +0900

    First Commit

---
+++ MeetingBoard/MeetingBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetingBoard
+//
+//  Created by 寺内 信夫 on 2015/12/22.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

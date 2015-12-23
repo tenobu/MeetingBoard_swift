4816ed1dc4e2f354eaaa67e6bb8cce83ec9484cc
寺内 信夫 <tenobu@gmail.com>
2015-12-23 17:09:02 +0900

    storyboard から自動的にコードを作る

---
+++ MeetingBoard/MeetingBoard/SellectMeeting.swift
@@ -0,0 +1,25 @@
+//
+//  SellectMeeting.swift
+//  MeetingBoard
+//
+//  Created by 寺内 信夫 on 2015/12/22.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SellectMeeting: UIViewController /*, UITableViewDataSource, UITableViewDelegate*/ {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
